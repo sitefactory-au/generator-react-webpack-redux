aab30a9808f63117d5be10098947d6d488ae99de
Matt Jobson <matt@sitefactory.com.au>
2016-07-27 17:01:22 +1000

    Fix to module scaffolding

--- generators/module/templates/index.js
+++ generators/module/templates/index.js
@@ -1,8 +1,7 @@
-import container from './container';
 import reducer from './reducer';
 import * as actions from './actions';
 import * as actionTypes from './actionTypes';
 import * as constants from './constants';
 import * as selectors from './selectors';
 
-export { container as default, reducer, actions, actionTypes, constants, selectors };
+export { selectors as default, reducer, actions, actionTypes, constants };
