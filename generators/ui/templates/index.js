5c0a307a7d305f9b300633e34a4b69f48ad64a9f
Luke Gardiner <luke@sitefactory.com.au>
2016-08-12 11:07:34 +1000

    Fix UI generator styles import path

--- generators/ui/templates/index.js
+++ generators/ui/templates/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { Card, CardText } from 'material-ui/Card';
-require('styles.less');
+require('./styles.less');
 
 export default () => <Card><CardText>Hello from <%= name %></CardText></Card>;
