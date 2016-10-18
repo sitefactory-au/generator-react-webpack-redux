39c96b0e28ef3907cdcd077b2a00d62eb1fc38e7
Matt Jobson <matt@sitefactory.com.au>
2016-10-18 16:18:11 +1100

    Added react-router-redux middleware

--- generators/root/templates/store.js
+++ generators/root/templates/store.js
@@ -2,13 +2,15 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import DevTools from '../utils/DevTools';
 import reducers from '../reducers';
+import { browserHistory } from 'react-router'; 
+import { routerMiddleware } from 'react-router-redux';
 
 module.exports = function(initialState) {
   let createStoreWithMiddleware;
   //if (__DEV__) {
   if (module.hot) {
     createStoreWithMiddleware = compose(
-      applyMiddleware(thunkMiddleware),
+      applyMiddleware(thunkMiddleware, routerMiddleware(browserHistory)),
       window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument()
       //persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
     )(createStore);
